c5914f340e5478273b3533f15f97a84336ea9539
devfardin <contactfardin22@gmail.com>
2025-02-15 02:43:31 +0600

    folter stracture

--- client/src/app/(WithCommonLayout)/layout.tsx
+++ client/src/app/(WithCommonLayout)/layout.tsx
@@ -1,4 +1,13 @@
+import Footer from "@/components/shared/Footer"
+import Navbar from "@/components/shared/Navbar"
+
 const CommonLayout = ({children}: {children: React.ReactNode}) => {
-  return <div>{children}</div>
+  return <div>
+    <Navbar/>
+    <main className="min-h-screen">
+    {children}
+    </main>
+    <Footer/>
+    </div>
 }
 export default CommonLayout
