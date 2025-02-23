ad8d513ed0fbac3707755c25f477422ddda08ae4
devfardin <contactfardin22@gmail.com>
2025-02-23 22:06:19 +0600

    update category type

--- client/src/components/modules/shop/category/index.tsx
+++ client/src/components/modules/shop/category/index.tsx
@@ -1,15 +1,34 @@
+import { NMTable } from "@/components/ui/core/NMTable"
 import CreateCategoryModal from "./CreateCategoryModal"
 import { ICategory } from "@/types"
+import { ColumnDef } from "@tanstack/react-table"
 
-type TCategoriesProps ={
+type TCategoriesProps = {
   categories: ICategory[]
-}
-const ManageCategory = async ({categories}: {categories: TCategoriesProps}) => {
+}[]
+const ManageCategory = async ({categories}: {categories: TCategoriesProps[]}) => {
+  const columns: ColumnDef<TCategoriesProps>[] = [
+    {
+      accessorKey: "name",
+      header: "Name",
+    },
+    {
+      accessorKey: "description",
+      header: "Description",
+    },
+    {
+      accessorKey: "amount",
+      header: "Amount",
+    },
+  ]
   return (
     <div>
         <div className="flex items-center justify-between gap-5 py-3 px-4 border rounded">
         <h1 className="text-xl font-medium text-neutral-600">Manage Category</h1>      
         <CreateCategoryModal/>
+        </div>
+        <div className="mt-5">
+          <NMTable data={categories} columns={columns}/>
         </div>
     </div>
   )
--- client/src/types/category.ts
+++ client/src/types/category.ts
@@ -3,4 +3,11 @@ export interface ICategory {
     name: string,
     description: string,
     icon: string,
+    parent: string | null,
+    isActive: string,
+    createdBy: string,
+    slug: string,
+    createdAt: string,
+    updatedAt: string,
+    children: ICategory[],
 }
